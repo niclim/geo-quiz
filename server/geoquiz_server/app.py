b29f8019bcfff3f07eb7c88093efbf3e742108ed
Nicholas Lim <18374483+niclim@users.noreply.github.com>
2018-06-27 11:53:37 +1200

    Added build move command and server setup

--- server/geoquiz_server/app.py
+++ server/geoquiz_server/app.py
@@ -1,14 +1,19 @@
 import os
 from flask import Flask, render_template
-
+from geoquiz_server import questions
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
-    # Serve react as catch all
+    register_blueprints(app)
+
+    # Serve react as catch all (in a production env)
     @app.route('/', defaults={'path': ''})
     @app.route('/<path:path>')
     def index(path):
         return render_template('index.html')
 
     return app
+
+def register_blueprints(app):
+    app.register_blueprint(questions.views.bp)
--- server/geoquiz_server/questions/views.py
+++ server/geoquiz_server/questions/views.py
@@ -1,11 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, Response, jsonify
 
 bp = Blueprint('questions', __name__, url_prefix='/api/questions')
 
 
 @bp.route('/<int:quiz_id>/<int:q_id>')
 def question(quiz_id, q_id):
-    pass
+    return jsonify(hi='hi')
 
 
 @bp.route('/answer/<int:quiz_id>/<q_id>')
@@ -13,6 +13,6 @@ def answer(quiz_id, q_id):
     pass
 
 
-@bp.route('/validate/quiz_id', methods=('POST'))
+@bp.route('/validate/quiz_id', methods=['POST'])
 def validate(quiz_id):
     pass
